721bc9ddb2cc9fd90ea9c0a1ec42bd420efaa718
SHAIK FAKRUDDIN <shaikfakruddin2145@gmail.com>
2024-06-03 21:18:42 +0530

    commit

--- react/assignment26/src/test1/Test1.jsx
+++ react/assignment26/src/test1/Test1.jsx
@@ -4,12 +4,11 @@ function Test1(props)
     return(
         <div className='card'>
         <img src={props.item.image} alt="" />
-            <h2>{props.item.title}</h2>
-        <p>{props.item.description}</p>
-        <p>{props.item.category}</p>
-        <p>{props.item.rating.rate}</p>
-        <p>{props.item.rating.count}</p>
-        <p>{props.item.price}</p>
+            <h2 cl>{props.item.title}</h2>
+        <p className='black'>{props.item.description}</p>
+        <p className='grey'>{props.item.category}</p>
+        <p className='bgrey'>Rating - {props.item.rating.rate} . {props.item.rating.count}</p>
+        <h3 className='price'>{props.item.price}</h3>
 
         </div>
     )
