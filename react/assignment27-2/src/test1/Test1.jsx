15daa5f329307dd3d6cb8b05d5417272af63eb0a
SHAIK FAKRUDDIN <shaikfakruddin2145@gmail.com>
2024-06-04 17:47:37 +0530

    commit

--- react/assignment27-2/src/test1/Test1.jsx
+++ react/assignment27-2/src/test1/Test1.jsx
@@ -4,11 +4,13 @@ function Test1(props){
 return(
   <div className='card'>
 <h2>{props.x.pid}</h2> 
-<img src={props.x.image} alt="" />
+<img className='main' src={props.x.image} alt="" />
+<img className='small' src={props.x.image1} alt="" />
+
 <h2>{props.x.title}</h2> 
-<h2>{props.x.description}</h2> 
-<h2>{props.x.place}</h2>
-<h2>{props.x.date}</h2>
+<p className='desc'>{props.x.description}</p> 
+<p className='place'>{props.x.place}</p>
+<p className='date'>{props.x.date}</p>
 
 
   </div>
